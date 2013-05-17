71ac153310ee68dcc120a801a4ac2d07604498b2
buzz@exotica.org.uk <buzz@exotica.org.uk@cdb62a16-12e6-4073-a101-aec4806a09d1>
2013-05-17 23:22:24 +0000

    updated: script.module.urlresolver to 1.0.9 + git d0708928 (https://github.com/Eldorados/script.module.urlresolver) added: xbmc4xbox compatibility patchset
    git-svn-id: https://svn.exotica.org.uk:8443/xbmc4xbox_addons@550 cdb62a16-12e6-4073-a101-aec4806a09d1

--- trunk/script.module.urlresolver/lib/urlresolver/plugins/stagevu.py
+++ trunk/script.module.urlresolver/lib/urlresolver/plugins/stagevu.py
@@ -25,6 +25,9 @@ import urllib2
 from urlresolver import common
 import os
 
+#SET ERROR_LOGO# THANKS TO VOINAGE, BSTRDMKR, ELDORADO
+error_logo = os.path.join(common.addon_path, 'resources', 'images', 'redx.png')
+
 class StagevuResolver(Plugin, UrlResolver, PluginSettings):
     implements = [UrlResolver, PluginSettings]
     name = "stagevu"
@@ -37,20 +40,30 @@ class StagevuResolver(Plugin, UrlResolver, PluginSettings):
 
 
     def get_media_url(self, host, media_id):
-        print 'stagevu: in get_media_url %s %s' % (host, media_id)
         web_url = self.get_url(host, media_id)
-        link = self.net.http_GET(web_url).content
-        p=re.compile('<embed type="video/divx" src="(.+?)"')
-        match=p.findall(link)
-        return match[0]
+        try:
+            link = self.net.http_GET(web_url).content
+            p=re.compile('<embed type="video/divx" src="(.+?)"')
+            match=p.findall(link)
+            if match:
+                return match[0]
+            else:
+                raise Exception ('File Not Found or removed')
+        except urllib2.URLError, e:
+            common.addon.log_error(self.name + ': got http error %d fetching %s' %
+                                   (e.code, web_url))
+            common.addon.show_small_popup('Error','Http error: '+str(e), 8000, error_logo)
+            return False
+        except Exception, e:
+            common.addon.log('**** Stagevu Error occured: %s' % e)
+            common.addon.show_small_popup(title='[B][COLOR white]STAGEVU[/COLOR][/B]', msg='[COLOR red]%s[/COLOR]' % e, delay=5000, image=error_logo)
+            return False 
 
     def get_url(self, host, media_id):
-        print 'stagevu: in get_url %s %s' % (host, media_id)
         return 'http://www.stagevu.com/video/%s' % media_id 
         
         
     def get_host_and_id(self, url):
-        print 'stagevu: in get_host_and_id %s' % (url)
         r = re.search('//(.+?)/video/([0-9a-zA-Z/]+)', url)
         if r:
             return r.groups()
@@ -59,6 +72,7 @@ class StagevuResolver(Plugin, UrlResolver, PluginSettings):
 
 
     def valid_url(self, url, host):
+        if self.get_setting('enabled') == 'false': return False
         return (re.match('http://(www.)?stagevu.com/video/' +
                          '[0-9A-Za-z]+', url) or
                          'stagevu' in host)
